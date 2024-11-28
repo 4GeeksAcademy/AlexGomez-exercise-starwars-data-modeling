8281d073426d1836bb937c53eba30019c4ad5271
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-11-28 12:56:02 +0000

    class vehiculo agregada

--- src/models.py
+++ src/models.py
@@ -34,7 +34,14 @@ class Personaje(Base):
     altura = Column(Integer, nullable=True)
     peso = Column(Integer, nullable=True)
     especie = Column(String, nullable=True)
-    
+class Vehiculo(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, unique=True, nullable=False)
+    modelo = Column(String, nullable=True)
+    costo = Column(Integer, nullable=True)
+    combustible = Column(Integer, nullable=True)
+    asientos = Column(Integer, nullable=True)
 
 class Favorito(Base):
     __tablename__ = 'favoritos'
@@ -43,30 +50,12 @@ class Favorito(Base):
     usuario_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
     planeta_id = Column(Integer, ForeignKey('planetas.id'), nullable=False)
     personaje_id = Column(Integer, ForeignKey('personajes.id'), nullable=False)
+    vehiculo_id = Column(Integer, ForeignKey('vehiculos.id'), nullable=False)
+
 
     usuario = relationship('usuario')
     planeta = relationship('planeta')
     personaje = relationship('personaje')
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
+    vehiculo = relationship('vehiculo')
 
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-
-#     def to_dict(self):
-#         return {}
-
-## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
